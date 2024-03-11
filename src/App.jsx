a5661121b82a6b615278fe828a92c5005502db6a
mohsensami <mohsensami88@gmail.com>
2024-03-11 20:42:04 +0330

    show single estate page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import HomePage from './routes/homePage/homePage';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './routes/layout/layout';
 import ListPage from './routes/listPage/listPage';
+import SinglePage from './routes/singlePage/singlePage';
 
 function App() {
     const router = createBrowserRouter([
@@ -16,6 +17,10 @@ function App() {
                 {
                     path: '/list',
                     element: <ListPage />,
+                },
+                {
+                    path: '/:id',
+                    element: <SinglePage />,
                 },
             ],
         },
