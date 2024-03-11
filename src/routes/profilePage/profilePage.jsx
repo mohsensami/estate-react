5380c9a7be8e0f6a4b201e9b04c5351b918ca651
mohsensami <mohsensami88@gmail.com>
2024-03-11 20:53:49 +0330

    estate list in profile page

--- src/routes/profilePage/profilePage.jsx
+++ src/routes/profilePage/profilePage.jsx
@@ -1,5 +1,5 @@
 import Chat from '../../components/chat/Chat';
-// import List from '../../components/list/List';
+import List from '../../components/list/List';
 import './profilePage.scss';
 
 function ProfilePage() {
@@ -30,11 +30,11 @@ function ProfilePage() {
                         <h1>My List</h1>
                         <button>Create New Post</button>
                     </div>
-                    {/* <List /> */}
+                    <List />
                     <div className="title">
                         <h1>Saved List</h1>
                     </div>
-                    {/* <List /> */}
+                    <List />
                 </div>
             </div>
             <div className="chatContainer">
